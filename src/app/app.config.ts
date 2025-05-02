ee881d7e94f61e5f869fd77d24c4c6968c60a704
eitan101 <eitan101@gmail.com>
2025-05-02 09:59:14 +0000

    firebase environment was updated + remove the importProvidersFrom

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,12 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
       provideFirebaseApp(() => initializeApp(environment.firebase)),
       provideFirestore(() => getFirestore()),
       provideAuth(() => getAuth()),
       provideFunctions(() => getFunctions()),
       provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
+      provideMessaging(() => getMessaging()),
     provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-b2b1d","appId":"1:139211931104:web:49c6fe3117d6e6200cbf85","storageBucket":"friendlychat-b2b1d.firebasestorage.app","apiKey":"AIzaSyAl8zUN6plL9TpHWjM8MDIgtEPXnm8lKxo","authDomain":"friendlychat-b2b1d.firebaseapp.com","messagingSenderId":"139211931104","measurementId":"G-0BYP1H9SEH"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,23 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: "AIzaSyAl8zUN6plL9TpHWjM8MDIgtEPXnm8lKxo",
+    authDomain: "friendlychat-b2b1d.firebaseapp.com",
+    projectId: "friendlychat-b2b1d",
+    storageBucket: "friendlychat-b2b1d.firebasestorage.app",
+    messagingSenderId: "139211931104",
+    appId: "1:139211931104:web:49c6fe3117d6e6200cbf85",
+    measurementId: "G-0BYP1H9SEH"
   },
+};
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAl8zUN6plL9TpHWjM8MDIgtEPXnm8lKxo",
+  authDomain: "friendlychat-b2b1d.firebaseapp.com",
+  projectId: "friendlychat-b2b1d",
+  storageBucket: "friendlychat-b2b1d.firebasestorage.app",
+  messagingSenderId: "139211931104",
+  appId: "1:139211931104:web:49c6fe3117d6e6200cbf85",
+  measurementId: "G-0BYP1H9SEH"
 };
